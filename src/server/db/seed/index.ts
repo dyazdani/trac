7ebad39bb6437aba484546e94c975c500f9c7d89
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-14 17:29:14 -0800

    Changed seed file to accomodate edits to prisma schema

--- src/server/db/seed/index.ts
+++ src/server/db/seed/index.ts
@@ -1,20 +1,33 @@
-const { PrismaClient } = require("@prisma/client");
-const { users, habits, checkIns, contacts } = require('./seedData.json');
+import { PrismaClient } from "@prisma/client";
+import { users, habits, routines} from './seedData.json';
 const prisma = new PrismaClient();
+
+interface HabitSeedData {
+    name: string
+    datesCompleted: string[]
+    checkInDay: string
+    ownerId: number
+}
+
+const habitsWithDateObjects = habits.map((habit: HabitSeedData) => {
+    return ({
+        ...habit,
+        datesCompleted: habit.datesCompleted.map((date: string) => new Date(date)) 
+    })
+})
+
+console.log("habitsWithDateObjects: ", habitsWithDateObjects);
 
 const load = async () => {
     try {
         await prisma.habit.deleteMany();
         console.log("Deleted records in habits table")
 
-        await prisma.checkIn.deleteMany();
-        console.log("Deleted records in checkIns table");
+        await prisma.routine.deleteMany();
+        console.log("Deleted records in routines table");
         
         await prisma.user.deleteMany();
         console.log("Deleted records in users table");
-
-        await prisma.contact.deleteMany();
-        console.log("Deleted records in contacts table");
 
         await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1;`;
         console.log("reset user auto increment to 1")
@@ -22,11 +35,8 @@ const load = async () => {
         await prisma.$executeRaw`ALTER SEQUENCE "Habit_id_seq" RESTART WITH 1;`;
         console.log("reset habit auto increment to 1")
 
-        await prisma.$executeRaw`ALTER SEQUENCE "CheckIn_id_seq" RESTART WITH 1;`;
-        console.log("reset checkIn auto increment to 1")
-
-        await prisma.$executeRaw`ALTER SEQUENCE "Contact_id_seq" RESTART WITH 1;`;
-        console.log("reset contact auto increment to 1")
+        await prisma.$executeRaw`ALTER SEQUENCE "Routine_id_seq" RESTART WITH 1;`;
+        console.log("reset routine auto increment to 1")
 
         await prisma.user.createMany({
             data: users
@@ -34,24 +44,21 @@ const load = async () => {
         console.log("Added user data");
 
         await prisma.habit.createMany({
-            data: habits
+            data: habitsWithDateObjects
         });
         console.log("Added habit data")
 
-        await prisma.checkIn.createMany({
-            data: checkIns
+        await prisma.routine.createMany({
+            data: routines
         });
-        console.log("Added checkIn data")
-
-        await prisma.contact.createMany({
-            data: contacts
-        });
-        console.log("Added contact data")
+        console.log("Added routine data")
 
     } catch(e) {
         console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
     } finally {
-        await prisma.$disconnect()
+        await prisma.$disconnect();
         process.exit(0);
     }
 }
