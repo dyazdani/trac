d0af905af59b47fd34ee91c0037bd9d2f7e7469e
Josh van Eyken <josh.vaneyken@gmail.com>
2023-12-18 13:55:17 -0800

    import Express and ViteExpress and set up basic apiRouter

---
+++ src/server/api/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const apiRouter = express.Router();
+
+// GET /api
+apiRouter.get("/", (req, res, next): void => {
+    try {
+        res.send("API is live");
+    } catch(e) {
+        next(e)
+    }
+});
+
+export default apiRouter;
