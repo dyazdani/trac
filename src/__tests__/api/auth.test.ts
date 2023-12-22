a534aac30156d779c5aaa3cd2cf95abb32623d31
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-22 12:37:35 -0800

    Added additional expects to auth.test.ts

--- src/__tests__/api/auth.test.ts
+++ src/__tests__/api/auth.test.ts
@@ -4,13 +4,9 @@ const request = supertest(app)
 import prisma from '../../utils/test/prisma.js';
 
 describe('api/auth', () => {
-    afterAll( async () => {
-        await prisma.$disconnect();
-    })
 
     describe('[POST] api/auth/register', () => {
         it('should respond with a `200` status code and user details', async () => {
-            await prisma.$connect();
             const { status, body } = await request
                 .post('/api/auth/register')
                 .send({
@@ -24,8 +20,12 @@ describe('api/auth', () => {
             expect(status).toBe(200)
             expect(newUser).not.toBeNull()
             expect(body.user.id).toBe(newUser?.id);
+            expect(body.user.dateCreated).toBeTruthy();
             expect(body.user.username).toBe('testusername');
             expect(body.user.email).toBe('test@email.com');
+            //excludePassword helper function should make password return undefined
+            expect(body.user.password).toBeUndefined();
+            expect(body.user.isAdmin).toBe(false);
         })
     })
 })
