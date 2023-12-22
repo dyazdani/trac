c441b4d7e2ba11959c37e1a3fe76f514a93d6066
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-21 22:33:37 -0800

    Added prisma-exclude to auth.ts

--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { prismaExclude } from "prisma-exclude";
+const bcrypt = require("bcrypt")
 import requireUser from "../../utils/requireUser.js";
 
 const SALT_ROUNDS = 10;
 
 const prisma = new PrismaClient();
+const exclude = prismaExclude(prisma);
+
 
 const {ACCESS_TOKEN_SECRET} = process.env;
 
@@ -13,6 +17,32 @@ const authRouter = express.Router();
 
 // POST /api/auth/register
 authRouter.post("/register", requireUser, async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        bcrypt.hash(password, SALT_ROUNDS, async function(err: Error | undefined, hash: string) {
+            if (err) next(err);
+            const user = await prisma.user.create({
+                data: {
+                    email,
+                    username,
+                    password: hash
+                }
+        })
+        // JSON Web Token returned to client
+        const token = jwt.sign({
+            username: user.username,
+            id: user.id,
+        }, ACCESS_TOKEN_SECRET);
+        
+        res.send({
+            token,
+            user: exclude("user", ["password"])
+        });
+    })
+    } catch (e) {
+        next(e)
+    }
+
 })
 
 
