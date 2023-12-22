e3820e55f225322e545e53a5d42925c7a2ee73b1
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-21 22:22:05 -0800

    Added requireUser middleware and added it to register endpoint

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import requireUser from "../../utils/requireUser.js";
 
 const SALT_ROUNDS = 10;
 
@@ -11,7 +12,7 @@ const usersRouter = express.Router();
 
 
 // POST /api/auth/register
-usersRouter.post("/register", async (req, res, next) => {
+usersRouter.post("/register", requireUser, async (req, res, next) => {
 })
 
 
