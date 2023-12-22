eaef7b841f5c585f6b1683cf643732cf5b40d026
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-22 03:31:07 -0800

    Changed jest config file

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: [
+    "node_modules", 
+    "src"
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
+
+export default jestConfig
