07716287fb0e8a3ab51539df779df954f4b6ddad
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-27 15:40:12 -0800

    Edited jwt auth middleware and server app to fix bugs and handle errors with semantic status codes in responses

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,5 +1,5 @@
 import {Request, Response, NextFunction} from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 declare module "jsonwebtoken" {
     export interface JwtPayload {
@@ -19,21 +19,23 @@ const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
     if (authHeader && authHeader.startsWith("Bearer ") && process.env.ACCESS_TOKEN_SECRET) {
         const token = authHeader.split(' ')[1];
         try {
-            const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) as JwtPayload
-
-            if (user) {
+            const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+            // Jwt should throw error if token cannot be verified,
+            // however jwt.verify also returns a string of the token that failed
+            // and only returns a JwtPayload object if successful.
+            // Therefore, this check for user being a string is a safeguard in case
+            // jwt does not throw error.
+            if (typeof user !== 'string') {
                 req.user = user
             } else {
                 req.user = null;
             }
-
-            next()
-
+            next();
         } catch (e) {
             next(e);
         } 
     } else {
-        next()
+        next(); 
     }
 }
 
