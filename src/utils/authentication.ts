62a104579b1f5e354edf8c5a54b429ee2a8b64d2
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-27 16:19:44 -0800

    Added 401 status code response when auth header in req is falsy

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -15,27 +15,36 @@ declare module "jsonwebtoken" {
 
 const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
+    let user;
 
     if (authHeader && authHeader.startsWith("Bearer ") && process.env.ACCESS_TOKEN_SECRET) {
         const token = authHeader.split(' ')[1];
         try {
-            const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+            user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
             // Jwt should throw error if token cannot be verified,
             // however jwt.verify also returns a string of the token that failed
             // and only returns a JwtPayload object if successful.
             // Therefore, this check for user being a string is a safeguard in case
             // jwt does not throw error.
-            if (typeof user !== 'string') {
+            if (user && typeof user !== 'string') {
                 req.user = user
-            } else {
-                req.user = null;
             }
+            console.log(req.user)
             next();
         } catch (e) {
+            if (!user) {
+                req.user = null;
+            }
+            console.log(req.user)
             next(e);
         } 
     } else {
-        next(); 
+        if (!user) {
+            req.user = null;
+        }
+        console.log("request: ", req.user)
+        res.status(401)
+        .send({name: "MissingAuthorizationHeader", message: "Request authorization header missing or has falsy value"})
     }
 }
 
