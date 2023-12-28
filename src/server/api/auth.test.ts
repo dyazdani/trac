f420ae588998cc35dd654ccb81da5b7ca8edb88f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-28 10:55:20 -0800

    Moved auth.test.ts to be next to auth.ts so that all tests would run in proper sequence

--- src/server/api/auth.test.ts
+++ src/server/api/auth.test.ts
@@ -1,5 +1,5 @@
 import supertest from 'supertest';
-import app from '../../server/app.js';
+import app from '../app.js';
 const request = supertest(app)
 import prisma from '../../utils/test/prisma.js';
 import jwt from 'jsonwebtoken';
