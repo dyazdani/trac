e8e0c88512427c7381aae0e6aae081fcfee988b7
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-29 11:30:54 -0800

    Moved auth.test.ts to a __tests__ folder

--- src/server/api/__tests__/auth.test.ts
+++ src/server/api/__tests__/auth.test.ts
@@ -1,7 +1,7 @@
 import supertest from 'supertest';
-import app from '../app.js';
+import app from '../../app.js';
 const request = supertest(app)
-import prisma from '../../utils/test/prisma.js';
+import prisma from '../../../utils/test/prisma.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 
