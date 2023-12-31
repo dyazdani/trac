dbb67861f4d4ece36244098a3fb01df22c3c4c42
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-30 16:49:27 -0800

    Added comment about strange TS error with import statement in App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import RegisterForm from './components/RegisterFOrm.js';
+// TODO: this import has a TS error when the "o" in the filepath is lowercase (i.e., "./components/RegisterForm.tsx"). WHY? 
+import RegisterForm from "./components/RegisterFOrm.js"
 
 const App: React.FC = () => {
 
