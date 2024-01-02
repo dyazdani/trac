5a313cd36aa709dbde779b6f19d714816085f5f1
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-02 12:02:47 -0800

    Delete unused code

--- src/client/components/LandingPage.tsx
+++ src/client/components/LandingPage.tsx
@@ -1,78 +1,71 @@
-import { useState } from 'react'
-import { 
-    Image,
-    Flex,
-    Spacer,
-    Hide,
-} from "@chakra-ui/react";
+import { useState } from "react";
+import { Image, Flex, Spacer, Hide } from "@chakra-ui/react";
 import RegisterForm from "./RegisterForm.js";
+import LoginForm from "./LoginForm.js";
 import mountainClimber from "../../../images/mountain-climber.jpg";
 
 const LandingPage = () => {
-    const [isLoginShowing, setIsLoginShowing] = useState(false)
+  const [isLoginShowing, setIsLoginShowing] = useState(false);
 
-    const handleLinkClick = () => {
-        setIsLoginShowing(!isLoginShowing)
-    }
+  const handleLinkClick = () => {
+    setIsLoginShowing(!isLoginShowing);
+  };
 
-    // Placeholder function until work on form submit begins
-    const handleSubmit = () => {
-        console.log("form submitted")
-    }
+  // Placeholder function until work on form submit begins
+  const handleSubmit = () => {
+    console.log("form submitted");
+  };
 
-    // To be used to toggle password visibility
-    const handleOnMouseDown = (e: React.MouseEvent<HTMLButtonElement>) =>  e.preventDefault();
-    
+  // To be used to toggle password visibility
+  const handleOnMouseDown = (e: React.MouseEvent<HTMLButtonElement>) =>
+    e.preventDefault();
 
-    return (
-        <Flex
-            alignItems="center"
-            w="100vw"
-            h="100vh"
-            bg="#b9eefe"
-        >
-            <Spacer />
-            <Hide
-                breakpoint='(max-width: 600px)'
-            >
-                <Image
-                    src={mountainClimber}
-                    alt="mountain climber scaling mountain"
-                    maxW="50vw"
-                    maxH="100%"
-                    h="100vh"
-                    objectFit="cover"
-                    borderRadius="50%"
-                    border="5px"
-                    bgColor="#564740"
-                    p="4"
-                    boxSize="35vw"
-                />
-                <Spacer /> 
-                <Spacer />
-            </Hide>
-                <RegisterForm 
+  return (
+    <Flex alignItems="center" w="100vw" h="100vh" bg="#b9eefe">
+      <Spacer />
+      <Hide breakpoint="(max-width: 600px)">
+        <Image
+          src={mountainClimber}
+          alt="mountain climber scaling mountain"
+          maxW="50vw"
+          maxH="100%"
+          h="100vh"
+          objectFit="cover"
+          borderRadius="50%"
+          border="5px"
+          bgColor="#564740"
+          p="4"
+          boxSize="35vw"
+        />
+        <Spacer />
+        <Spacer />
+      </Hide>
+      {/* <RegisterForm 
                     handleLinkClick={handleLinkClick}
                     handleSubmit={handleSubmit}
                     handleOnMouseDown={handleOnMouseDown}    
-                />
-        
-            {/* The following commented out code will replace the line above once a LoginForm is created. */}
-            {/* {isLoginShowing ? 
-            <LoginForm
-                handleLinkClick={handleLinkClick}
-            /> :  
-            <RegisterForm 
-                handleLinkClick={handleLinkClick}
-            />} */}
-            <Hide
-                breakpoint='(max-width: 600px)'
-            >
-                <Spacer />    
-            </Hide>
-            <Spacer /> 
-        </Flex>
-    )
-}
+                /> */}
+
+      {/* The following commented out code will replace the line above once a LoginForm is created. */}
+      {isLoginShowing ? (
+        <LoginForm
+          handleLinkClick={handleLinkClick}
+          handleSubmit={handleSubmit}
+          handleOnMouseDown={handleOnMouseDown}
+        />
+      ) : (
+        <RegisterForm
+          handleLinkClick={handleLinkClick}
+          handleSubmit={handleSubmit}
+          handleOnMouseDown={handleOnMouseDown}
+        />
+      )}
+      <Hide breakpoint="(max-width: 600px)">
+        <Spacer />
+      </Hide>
+      <Spacer />
+    </Flex>
+  );
+};
 
 export default LandingPage;
--- src/client/components/LoginForm.tsx
+++ src/client/components/LoginForm.tsx
@@ -1,160 +1,106 @@
 import React, { useState } from "react";
-import { 
-    FormControl, 
-    Input, 
-    FormLabel, 
-    Box,
-    VStack,
-    Card, 
-    CardHeader, 
-    CardBody, 
-    CardFooter,
-    Heading,
-    Text,
-    Button,
-    Link,
-    InputGroup,
-    InputRightElement,
-    IconButton,
-    ButtonGroup
+import {
+  FormControl,
+  Input,
+  FormLabel,
+  Box,
+  VStack,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+  Link,
+  InputGroup,
+  InputRightElement,
+  IconButton,
 } from "@chakra-ui/react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"
-// import { useRegisterMutation } from "../features/api.js";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 export interface LoginFormProps {
-    handleLinkClick: () => void
-    handleSubmit: () => void
-    handleOnMouseDown: (e: React.MouseEvent<HTMLButtonElement>) => void
+  handleLinkClick: () => void;
+  handleSubmit: () => void;
+  handleOnMouseDown: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({
-    handleLinkClick, 
-    handleSubmit,
-    handleOnMouseDown
+  handleLinkClick,
+  handleSubmit,
+  handleOnMouseDown,
 }) => {
-    // const [register, { isLoading, isError, data}] = useRegisterMutation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [email, setEmail] = useState("");
-    // const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    // const [confirmPassword, setConfirmPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
-    // const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  return (
+    <Card variant="elevated" align="center" size="md" m="4">
+      <CardHeader>
+        <Heading>trac</Heading>
+        <Text>Stay on trac by logging in.</Text>
+      </CardHeader>
+      <CardBody>
+        <Box as="form" onSubmit={handleSubmit}>
+          <VStack as="fieldset">
+            <FormControl>
+              <FormLabel>Email Address</FormLabel>
+              <Input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+            </FormControl>
 
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <InputGroup size="md">
+                <Input
+                  pr="4.5rem"
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                />
+                <InputRightElement width="2.5rem">
+                  <IconButton
+                    size="sm"
+                    h="1.75rem"
+                    icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword((show) => !show)}
+                    onMouseDown={handleOnMouseDown}
+                    data-testid="password-visibility-button"
+                  />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
 
-    return (
-        <Card
-            variant="elevated"
-            align="center"
-            size="lg" 
-            m="4"
-        >
-            <CardHeader>
-                <Heading>trac</Heading>
-                <Text>Stay on trac by logging in.</Text>
-            </CardHeader>
-            <CardBody>
-                <Box
-                    as="form"
-                    onSubmit={handleSubmit}
-                >
-                    <VStack
-                        as="fieldset"
-                    >
-                        <FormControl>
-                            <FormLabel>Email Address</FormLabel>
-                            <Input 
-                                type='email' 
-                                onChange={e => setEmail(e.target.value)}
-                                value={email}
-                                required
-                            />
-                        </FormControl>
-                        {/* <FormControl>
-                            <FormLabel>Username</FormLabel>
-                            <Input 
-                                type='username' 
-                                onChange={e => setUsername(e.target.value)}
-                                value={username}
-                                required
-                            />
-                        </FormControl> */}
-                        <FormControl>
-                            <FormLabel>Password</FormLabel>
-                                <InputGroup size="md">
-                                    <Input
-                                        pr="4.5rem" 
-                                        type={showPassword ? "text" : "password"}
-                                        onChange={e => setPassword(e.target.value)}
-                                        value={password}
-                                        required
-                                    />
-                                    <InputRightElement width="2.5rem">
-                                        <IconButton 
-                                            size="sm"
-                                            h="1.75rem"
-                                            icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                                            aria-label="toggle password visibility"
-                                            onClick={() => setShowPassword((show) => !show)}
-                                            onMouseDown={handleOnMouseDown}
-                                            data-testid="password-visibility-button"
-                                        />
-                                    </InputRightElement>
-                                </InputGroup>
-                        </FormControl>
-                        {/* <FormControl>
-                            <FormLabel>Confirm Password</FormLabel>
-                            <InputGroup size="md">
-                                    <Input
-                                        pr="4.5rem" 
-                                        type={showConfirmPassword ? "text" : "password"}
-                                        onChange={e => setConfirmPassword(e.target.value)}
-                                        value={confirmPassword}
-                                        required
-                                    />
-                                    <InputRightElement width="2.5rem">
-                                        <IconButton 
-                                            size="sm"
-                                            h="1.75rem"
-                                            icon={showConfirmPassword ? <ViewOffIcon /> : <ViewIcon />}
-                                            aria-label="toggle password visibility"
-                                            onClick={() => setShowConfirmPassword((show) => !show)}
-                                            onMouseDown={handleOnMouseDown}
-                                            data-testid="confirm-password-visibility-button"
-                                        />
-                                    </InputRightElement>
-                                </InputGroup>
-                        </FormControl> */}
-                        <ButtonGroup>
-                            <Button
-                                colorScheme="yellow"
-                                data-testid="submit-button"
-                                type="submit"    
-                            >
-                                <Text>Log In</Text>
-                            </Button> 
-                            <Button
-                                colorScheme="yellow"
-                                variant='outline'
-                                data-testid="submit-button"
-                                type="submit"    
-                            >
-                                <Text>Log In With Demo User</Text>
-                            </Button> 
-                        </ButtonGroup>
-                    </VStack>
-                </Box>    
-            </CardBody>
-            <CardFooter>
-                <Text> 
-                    Don't have an account? <Link data-testid="signup-link" onClick={handleLinkClick} color="teal">Sign Up.</Link>
-                </Text>
-            </CardFooter>
-        </Card>
-    )
-}
-
-
-
+            <Button
+              colorScheme="yellow"
+              data-testid="submit-button"
+              type="submit"
+            >
+              <Text>Log In</Text>
+            </Button>
+          </VStack>
+        </Box>
+      </CardBody>
+      <CardFooter>
+        <Text>
+          Don't have an account?{" "}
+          <Link
+            data-testid="signup-link"
+            onClick={handleLinkClick}
+            color="teal"
+          >
+            Sign Up.
+          </Link>
+        </Text>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default LoginForm;
