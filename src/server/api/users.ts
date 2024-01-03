019de7a393a26e28140dd606252dc1a9507f4a8d
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-03 11:33:39 -0800

    Added endpoint for POST api/users/:id/habits

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -2,6 +2,7 @@ import express from "express";
 import excludePassword from "../../utils/excludePassword.js";
 import prisma from "../../utils/test/prisma.js";
 import requireUser from "../../utils/requireUser.js";
+import { createHabitReqBody } from "../../types/index.js";
 
 const usersRouter = express.Router();
 
@@ -13,6 +14,50 @@ usersRouter.get("/", requireUser, async (req, res, next): Promise<void> => {
     } catch (e) {
         next(e);
     }
+})
+
+// POST /api/users/:id/habits
+usersRouter.post("/:id/habits", requireUser, async (req, res, next): Promise<void> => {
+    if (req.user) {
+        try {
+            const { 
+                name,
+                routineDays, 
+                checkInDay
+            }: createHabitReqBody = req.body
+
+            // Create Habit
+            const ownerId = Number(req.params.id);
+
+            const habit = await prisma.habit.create({
+                data: {
+                    name,
+                    ownerId,
+                }
+            });
+
+            // Create Routine associated with Habit
+            const routine = await prisma.routine.create({
+                data: {
+                    habitId: habit.id, 
+                    ...routineDays
+                }
+            })
+
+            // Create CheckIn associated with Habit
+            const checkIn = await prisma.checkIn.create({
+                data: {
+                    habitId: habit.id, 
+                    dayOfTheWeek: checkInDay
+                }
+            })
+
+            res.send({ habit });
+        } catch (e) {
+            next(e);
+        }
+    }
+
 })
 
 export default usersRouter;
