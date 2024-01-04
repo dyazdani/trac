68bc50bcc24febe58443fd56a2cac8ce31b7af28
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-04 11:01:40 -0800

    Removed Create Habit button for testing mutation

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -1,27 +1,11 @@
 import { Box, Button } from "@chakra-ui/react";
 import { useAppDispatch } from "../app/hooks.js";
 import { logout } from "../features/authSlice.js";
-import { useCreateHabitMutation } from "../features/api.js";
-import { DayOfTheWeek } from "@prisma/client";
 import RightDrawer from "./RightDrawer.js";
 
-const habitDetails = {
-    name: `"THIS IS A TEST"`,
-    routineDays: {
-        monday: false,
-        tuesday: false,
-        wednesday: true,
-        thursday: false,
-        friday: true,
-        saturday: false,
-        sunday: false
-    },
-    checkInDay: DayOfTheWeek.MONDAY
-}
 
 const Dashboard = () => {
     const dispatch = useAppDispatch()
-    const [createHabit, {data, isLoading, error}] = useCreateHabitMutation();
 
     return (
         <Box
@@ -35,24 +19,6 @@ const Dashboard = () => {
                 onClick={() => {dispatch(logout())}}
             >
                 Logout
-            </Button>
-            <Button
-                type="button"
-                onClick={async (e) => {
-                    e.preventDefault();
-                    if (!isLoading) {
-                        const data = await createHabit({id: 2, habitDetails});
-                        if (error) {
-                            console.error(error);
-                        } 
-                        if (data) {
-                            console.log(data);
-                        }
-                    }
-
-                }}
-            >
-                Create Habit
             </Button>
             <RightDrawer />
         </Box>
