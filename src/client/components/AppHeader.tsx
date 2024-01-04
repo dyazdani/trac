9c6cda812d9979e71bb2f5a359031c26baf12f9a
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-03 18:48:38 -0800

    Add text to AppHeader

--- src/client/components/AppHeader.tsx
+++ src/client/components/AppHeader.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { 
+    HStack, 
+    Box, 
+    Text 
+} from "@chakra-ui/react";
 
-type AppHeaderProps = {}
+type AppHeaderProps = {};
 
-const AppHeader = (props: AppHeaderProps) =>  {
-    return(
-        <>
-            <div>I am the AppHeader component</div>
-        </>
-    );
-}
+const AppHeader = (props: AppHeaderProps) => {
+  return (
+    <>
+      <Box 
+        bg="#b9eefe" 
+        w="100%" 
+        p={4}
+        >
+        <HStack>
+          <Text fontSize='2xl'>trac</Text>
+        </HStack>
+      </Box>
+    </>
+  );
+};
 
 export default AppHeader;
