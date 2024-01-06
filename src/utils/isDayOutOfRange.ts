27178fbbbbbd0a419877a0031ca24bdec7fdb058
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-06 04:03:12 -0800

    renamed isDateOutOfRange

--- src/utils/isDayOutOfRange.ts
+++
@@ -1,9 +0,0 @@
-const isDayOutOfRange = (start: Date, end: Date, target: Date) => {
-    const startDay = start.getTime();
-    const endDay = end.getTime();
-    const targetDay = target.getTime();
-
-    return targetDay > startDay && targetDay < endDay;
-}
-
-export default isDayOutOfRange;
