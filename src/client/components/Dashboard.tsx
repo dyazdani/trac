37ed6baafd78b8e397a20976da5117504c4e0c46
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-06 14:46:34 -0800

    Added test query to Dashboard.tsx to console log successful getHabitById query

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -1,16 +1,29 @@
 import { Box } from "@chakra-ui/react";
 import RightDrawer from "./RightDrawer.js";
 import UpdateHabitButton from "./UpdateHabitButton.js";
-import { useGetHabitsByUserQuery } from "../features/api.js";
+import { 
+    useGetHabitsByUserQuery, 
+    useGetHabitByIdQuery 
+} from "../features/api.js";
 import { useAppSelector } from "../app/hooks.js";
 
 const Dashboard = () => {
   const currentUser = useAppSelector((state) => state.auth.user);
 
+  // TODO: Will need to remove habit variable when cleaning up code and finalizing dshboard
   let habits;
+  let habit
   if (currentUser) {
     const { data } = useGetHabitsByUserQuery(currentUser.id);
     habits = data?.habits || [];
+    
+    const { data: habitData } = useGetHabitByIdQuery({
+        id: 2,
+        habitId: 5
+    })
+    habit = habitData?.habit
+
+    console.log(habit)
   }
 
     return (
