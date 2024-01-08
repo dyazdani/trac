21891b9b1bc235ef01e6cb75e683f4ec7c3ac8f5
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-07 18:07:12 -0800

    Create habit component

---
+++ src/client/components/Habit.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type HabitProps = {}
+
+const Habit = (props: HabitProps) =>  {
+    return(
+        <>
+            <div>I am the Habit component</div>
+        </>
+    );
+}
+
+export default Habit;
