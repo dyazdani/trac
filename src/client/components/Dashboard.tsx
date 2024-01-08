d4689e6d3c9f953b9158d758cdf8ebce4065362d
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-08 10:51:05 -0800

    Commented out unnecessary code

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -13,20 +13,20 @@ import { useAppSelector } from "../app/hooks.js";
 const Dashboard = () => {
   
   // TODO: Will need to remove habit variable when cleaning up code and finalizing dshboard
-  let habits;
-  let habit
-  if (currentUser) {
-    const { data } = useGetHabitsByUserQuery(currentUser.id);
-    habits = data?.habits || [];
+  // let habits;
+  // let habit
+  // if (currentUser) {
+  //   const { data } = useGetHabitsByUserQuery(currentUser.id);
+  //   habits = data?.habits || [];
     
-    const { data: habitData } = useGetHabitByIdQuery({
-        id: 2,
-        habitId: 5
-    })
-    habit = habitData?.habit
+  //   const { data: habitData } = useGetHabitByIdQuery({
+  //       id: 2,
+  //       habitId: 5
+  //   })
+  //   habit = habitData?.habit
 
-    console.log(habit)
-  }
+  //   console.log(habit)
+  // }
 
   return (
     <>
