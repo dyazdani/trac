6ba9d810f60a943410b74e8c465b2d1302be8b89
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-10 13:57:08 -0800

    Add DeleteHabitButton component

---
+++ src/client/components/DeleteHabitButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type DeleteHabitButtonProps = {}
+
+const DeleteHabitButton = (props: DeleteHabitButtonProps) =>  {
+    return(
+        <>
+            <div>I am the DeleteHabitButton component</div>
+        </>
+    );
+}
+
+export default DeleteHabitButton;
