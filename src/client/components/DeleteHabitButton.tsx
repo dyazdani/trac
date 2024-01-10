e9214139fb39543b015d4daede8c9431bb274e3b
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-10 14:21:37 -0800

    Add delete habit functionality

--- src/client/components/DeleteHabitButton.tsx
+++ src/client/components/DeleteHabitButton.tsx
@@ -15,7 +15,17 @@ type DeleteHabitButtonProps = {
     habit: HabitWithDetails
 }
 
-const DeleteHabitButton = (props: DeleteHabitButtonProps) =>  {
+const DeleteHabitButton = ({ habit }: DeleteHabitButtonProps) =>  {
+  const currentUser = useAppSelector(state => state.auth.user);
+  const [deleteHabit, { isSuccess, isLoading, isError, error }] = useDeleteHabitMutation();
+  const toast = useToast();
+
+  const handleDeleteHabit = async () => {
+    if (currentUser) {
+      const deletedHabit = await deleteHabit({id: currentUser.id, habitId: habit.id})
+      console.log(deletedHabit, "THE DELETED HABIT")
+    }
+  }
     
     return(
         <>
@@ -23,6 +33,7 @@ const DeleteHabitButton = (props: DeleteHabitButtonProps) =>  {
                 aria-label="delete-habit-button" 
                 icon={<DeleteIcon />} 
                 variant="unstyled"
+                onClick={handleDeleteHabit}
             />
         </>
     );
