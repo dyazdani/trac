a69823965248f4c9f2f7cd463a7e5102b1441fb6
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-09 21:41:07 -0800

    Added helper function to get array of day of the week strings to store as default checkbox value for edit habit form

---
+++ src/utils/getRoutineDaysStringArray.ts
@@ -0,0 +1,20 @@
+import { Routine } from "@prisma/client";
+import { RoutineDaysArrayType, DayString } from "../types/index.js";
+
+const getRoutineDaysStringArray = (routine: Routine) => {
+    const routineDays = (({ id, habitId, dateCreated, dateUpdated, ...object }) => object)(routine);
+
+    let finalArray: RoutineDaysArrayType = [];
+
+    for (const key in routineDays) {
+        if (routineDays[key as keyof typeof routineDays]) {
+            const dayString: DayString = key as DayString
+            finalArray = [...finalArray, dayString]
+        }
+    }
+    console.log(finalArray)
+    return finalArray
+}
+
+export default getRoutineDaysStringArray;
+
