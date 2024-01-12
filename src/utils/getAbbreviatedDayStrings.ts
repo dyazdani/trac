327fc479498312f16f454c615634e8031aa87dab
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-12 10:06:12 -0800

    Created helper function to get array of days of the week string that is compatibke with Knock API

---
+++ src/utils/getAbbreviatedDayStrings.ts
@@ -0,0 +1,38 @@
+import { DaysOfWeek } from "@knocklabs/node";
+import { RoutineDaysArrayType } from "./getBooleanRoutineDays.js";
+
+export type AbbreviatedDayString = 
+    "mon" |
+    "tue" |
+    "wed" |
+    "thu" |
+    "fri" |
+    "sat" |
+    "sun"
+
+export type KnockDaysOfWeekType = {
+    monday: DaysOfWeek
+    tuesday: DaysOfWeek
+    wednesday: DaysOfWeek
+    thursday: DaysOfWeek
+    friday: DaysOfWeek
+    saturday: DaysOfWeek
+    sunday: DaysOfWeek
+} 
+
+const getAbbreviatedDayStrings = (dayStringArray: RoutineDaysArrayType): DaysOfWeek[] => {
+    
+const knockDaysOfWeek: KnockDaysOfWeekType = {
+    monday: DaysOfWeek.Mon,
+    tuesday: DaysOfWeek.Tue,
+    wednesday: DaysOfWeek.Wed,
+    thursday: DaysOfWeek.Thu,
+    friday: DaysOfWeek.Fri,
+    saturday: DaysOfWeek.Sat,
+    sunday: DaysOfWeek.Sun
+}
+    
+    return dayStringArray.map(day => knockDaysOfWeek[day])
+}
+
+export default getAbbreviatedDayStrings;
