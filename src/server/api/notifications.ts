af6d9cdc65b65befc5d969bfb0d19cc377ef4f6c
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-11 23:09:00 -0800

    Added identify user endpoint

--- src/server/api/notifications.ts
+++ src/server/api/notifications.ts
@@ -1,8 +1,29 @@
 import express from "express";
 import { Knock } from "@knocklabs/node";
+import requireUser from "../../utils/requireUser.js";
 
 const notificationsRouter = express.Router();
 
 const knock = new Knock(process.env.KNOCK_API_KEY);
+
+// "Identify User" PUT api/notifications/users/:user_id
+notificationsRouter.put("/users/:user_id", requireUser, async (req, res, next): Promise<void> => {
+    if (req.user) {
+        try {
+            const id = req.params.user_id
+            const { email, username } = req.body;
+            if (!id) {
+                console.log("id: ", id)
+            }
+            const user = await knock.users.identify(id, {
+                email,
+                username
+            })
+            res.send({user});
+        } catch (e) {
+            next(e);
+        }
+    }
+})
 
 export default notificationsRouter;
