d668ec369f6288ef0d7ca211a24947ae7c18f58f
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-12 14:44:53 -0800

    Modified create schedule endpoint to get userId from req.body. Added TODO to change this back to req.user.id when branch is being finalized

--- src/server/api/notifications.ts
+++ src/server/api/notifications.ts
@@ -11,8 +11,11 @@ const knock = new Knock(process.env.KNOCK_API_KEY);
 notificationsRouter.post("/schedules", requireUser, async (req, res, next) => {
     if (req.user) {
         try {
-            const userId = String(req.user.id)
+            //TODO: change the userId back to being obtained by req.user.id once the mutation is able to be tested on this branch
+            // How userId was obtained was changed to req.body because Postman does not have a way to set user property on request
+            // const userId = String(req.user.id)
             const { 
+                userId,
                 habitName,
                 days,
                 workflowKey
