8e8d38c0b80a9496ae99652f2d832e2aa1d62b70
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-17 13:53:42 -0800

    Reversed order of status report entries

--- src/utils/getStatusReport.ts
+++ src/utils/getStatusReport.ts
@@ -9,22 +9,22 @@ const getStatusReport = (habit: HabitWithDetails) => {
     if (checkInDate) {
 
         const statusReport = [
-            {[checkInDate.toDateString()]: false},
-            {[new Date(checkInDate.getTime() - ONE_DAY_IN_MILLISECONDS).toDateString()]: false},
-            {[new Date(checkInDate.getTime() - (2 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false},
-            {[new Date(checkInDate.getTime() - (3 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false},
-            {[new Date(checkInDate.getTime() - (4 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false},
-            {[new Date(checkInDate.getTime() - (5 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false},
-            {[new Date(checkInDate.getTime() - (6 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false}
+            {[new Date(checkInDate.getTime() - (6 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: "Not Completed"},
+            {[new Date(checkInDate.getTime() - (5 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: "Not Completed"},
+            {[new Date(checkInDate.getTime() - (4 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: "Not Completed"},
+            {[new Date(checkInDate.getTime() - (3 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: "Not Completed"},
+            {[new Date(checkInDate.getTime() - (2 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: "Not Completed"},
+            {[new Date(checkInDate.getTime() - ONE_DAY_IN_MILLISECONDS).toDateString()]: "Not Completed"},
+            {[checkInDate.toDateString()]: "Not Completed"}
         ]
         let currentDateInMilliseconds = checkInDate?.getTime();
     
-        console.log(`${habit.name} datesCompleted: ${habit.datesCompleted}`)
+        console.log("datesCompleted: ", habit.datesCompleted)
 
-        for (let i = 0; i < 7; i++) {
+        for (let i = 6; i > -1; i--) {
             const targetDate = new Date(currentDateInMilliseconds);
             if (habit.datesCompleted.find(date => areDatesSameDayMonthYear(new Date(date), targetDate))) {
-                statusReport[i][targetDate.toDateString()] = true
+                statusReport[i][targetDate.toDateString()] = "Completed"
             }
             currentDateInMilliseconds = currentDateInMilliseconds - ONE_DAY_IN_MILLISECONDS;
         }
