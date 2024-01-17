fcd5beb5c417cabfa4fcd6e584d72e3014fcc736
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-17 13:58:26 -0800

    Added util function to get default status report message string to populate email body text area component

---
+++ src/utils/getDefaultStatusReportMessage.ts
@@ -0,0 +1,43 @@
+import { HabitWithDetails } from "../types/index.js";
+import getStatusReport from "./getStatusReport.js";
+
+const getDefaultStatusReportMessage = (habit: HabitWithDetails, username: string) => {
+
+    const statusReport = getStatusReport(habit);
+    
+    if (statusReport) {
+        let statusReportString = ""
+    
+        for (let i = 0; i < statusReport?.length; i++) {
+            statusReportString = `${statusReportString}${Object.keys(statusReport[i]).join('')}: ${Object.values(statusReport[i]).join('')}\n\t`
+        }
+
+        return (`Hi Friends!
+    I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+
+        Status Report\n\t${statusReportString} 
+    
+Thank you for helping to keep me accountable and motivated! I'll be sending another status report next week.
+
+    
+Onward and Upward,
+${username}
+    `)
+    } else {
+        return (`Hi Friends!
+    I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+
+        Status Report
+    
+Thank you for helping to keep me accountable and motivated! I'll be sending another status report next week.
+
+    
+Onward and Upward,
+${username}
+    `)
+    }
+    
+}
+
+
+export default getDefaultStatusReportMessage;
