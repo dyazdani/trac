dacd824c905811041b3729f40abe96ec7de7f67e
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-17 12:41:06 -0800

    Added util function to get status report object for the most recent week before the most recent check-in day

---
+++ src/utils/getStatusReport.ts
@@ -0,0 +1,35 @@
+import { HabitWithDetails } from "../types/index.js";
+import areDatesSameDayMonthYear from "./areDatesSameDayMonthYear.js";
+import getMostRecentCheckInDayDate, { ONE_DAY_IN_MILLISECONDS } from "./getMostRecentCheckInDayDate.js";
+
+
+const getStatusReport = (habit: HabitWithDetails) => {
+    const checkInDate = getMostRecentCheckInDayDate(habit);
+
+    if (checkInDate) {
+
+        const statusReport = {
+            [checkInDate.toDateString()]: false,
+            [new Date(checkInDate.getTime() - ONE_DAY_IN_MILLISECONDS).toDateString()]: false,
+            [new Date(checkInDate.getTime() - (2 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false,
+            [new Date(checkInDate.getTime() - (3 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false,
+            [new Date(checkInDate.getTime() - (4 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false,
+            [new Date(checkInDate.getTime() - (5 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false,
+            [new Date(checkInDate.getTime() - (6 * ONE_DAY_IN_MILLISECONDS)).toDateString()]: false
+        }
+        let currentDateInMilliseconds = checkInDate?.getTime();
+    
+        console.log(`${habit.name} datesCompleted: ${habit.datesCompleted}`)
+
+        for (let i = 0; i < 7; i++) {
+            const targetDate = new Date(currentDateInMilliseconds);
+            if (habit.datesCompleted.find(date => areDatesSameDayMonthYear(new Date(date), targetDate))) {
+                statusReport[targetDate.toDateString()] = true
+            }
+            currentDateInMilliseconds = currentDateInMilliseconds - ONE_DAY_IN_MILLISECONDS;
+        }
+        return statusReport;
+    } 
+}
+
+export default getStatusReport;
