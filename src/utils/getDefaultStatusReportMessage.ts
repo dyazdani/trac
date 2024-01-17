46cd969f64cf23edba9824298c0fd068b8513d65
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-17 14:30:19 -0800

    Fixing formatting of status report string returned by util function

--- src/utils/getDefaultStatusReportMessage.ts
+++ src/utils/getDefaultStatusReportMessage.ts
@@ -12,32 +12,31 @@ const getDefaultStatusReportMessage = (habit: HabitWithDetails, username: string
             statusReportString = `${statusReportString}${Object.keys(statusReport[i]).join('')}: ${Object.values(statusReport[i]).join('')}\n\t`
         }
 
-        return (`Hi Friends!
-    I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+    return (
+        `Hi Friends!
+        I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+        
+            Status Report
+        ${statusReportString} 
 
-        Status Report\n\t${statusReportString} 
-    
 Thank you for helping to keep me accountable and motivated! I'll be sending another status report next week.
 
-    
+
 Onward and upward,
-${username}
-    `)
+${username}`)
     } else {
-        return (`Hi Friends!
-    I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+        return(
+        `Hi Friends!
+        I've been using the Trac app to record how frequently I am completing practice for my ${habit.name} habit. I would like to send you a report of how I did over the week leading up to my most recent weekly check-in day. Here's how I did:
+        
+            Status Report
 
-        Status Report
-    
 Thank you for helping to keep me accountable and motivated! I'll be sending another status report next week.
 
-    
-Onward and upward,
-${username}
-    `)
-    }
-    
-}
 
+Onward and upward,
+${username}`)
+    }
+}
 
 export default getDefaultStatusReportMessage;
