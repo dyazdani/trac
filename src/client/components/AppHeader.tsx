2f50a53a6043cc67eef5a12dbc2539d5f6954e67
Josh van Eyken <josh.vaneyken@gmail.com>
2024-01-17 17:22:34 -0800

    Wrap KnockFeedProvider around MessagesMenu and pass in api keys and user id

--- src/client/components/AppHeader.tsx
+++ src/client/components/AppHeader.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { useAppDispatch } from "../app/hooks.js";
+import { 
+  useAppDispatch, 
+  useAppSelector 
+} from "../app/hooks.js";
 import { logout } from "../features/authSlice.js";
 import {
   KnockFeedProvider
@@ -18,7 +21,12 @@ type AppHeaderProps = {};
 
 const AppHeader = (props: AppHeaderProps) => {
   const dispatch = useAppDispatch()
+  const currentUser = useAppSelector(state => state.auth.user);
 
+  console.log(process.env, "Node environment object")
+  console.log(process.env.ACCESS_TOKEN_SECRET, "ACCESS TOKEN")
+  console.log(process.env.KNOCK_API_KEY, "KNOCK API KEY")
+  
   return (
     <>
       <Box 
@@ -33,7 +41,17 @@ const AppHeader = (props: AppHeaderProps) => {
         <HStack>
           <Text fontSize='2xl'>trac</Text>
             <Spacer/>
-            <MessagesMenu />
+            {process.env.KNOCK_API_KEY && 
+            process.env.KNOCK_PUBLIC_API_KEY && 
+            currentUser &&
+              <KnockFeedProvider
+                apiKey={process.env.KNOCK_PUBLIC_API_KEY}
+                feedId={process.env.KNOCK_API_KEY}
+                userId={currentUser?.id.toString()}
+              >
+                <MessagesMenu />
+              </KnockFeedProvider>
+            }
             <Button
                 type="button"
                 onClick={() => {dispatch(logout())}}
