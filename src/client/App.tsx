59c516779ec3aaa850aace5f54e1ffee7d351664
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-18 23:40:16 -0800

    Moved App Header to Dashboard component and added CTABanner to Dashboard

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,20 +2,18 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage.js';
 import Dashboard from './components/Dashboard.js';
-import AppHeader from './components/AppHeader.js';
 import { useAppSelector } from './app/hooks.js';
 
 const App: React.FC = () => {
-const currentUser = useAppSelector((state) => state.auth.user);
+  const currentUser = useAppSelector((state) => state.auth.user);
 
-  return (
-    <>
-      {currentUser && <AppHeader />}
-      <Routes>
-        <Route path="/" element={currentUser ? <Dashboard /> : <LandingPage />}/>
-      </Routes>
-    </>
-  );
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={currentUser ? <Dashboard /> : <LandingPage />}/>
+        </Routes>
+      </>
+    );
 }
 
 export default App;
--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -1,18 +1,39 @@
 import { Box } from "@chakra-ui/react";
 import RightDrawer from "./RightDrawer.js";
 import MyHabits from "./MyHabits.js";
-import UpdateHabitButton from "./UpdateHabitButton.js";
-import HabitCard from "./HabitCard.js";
-import { 
-    useGetHabitsByUserQuery, 
-    useGetHabitByIdQuery 
-} from "../features/api.js";
 import { useAppSelector } from "../app/hooks.js";
+import { useGetHabitsByUserQuery } from "../features/api.js";
+import AppHeader from "./AppHeader.js";
+import CTABanner from "./CTABanner.js";
+import isTodayCheckInDay from "../../utils/isTodayCheckInDay.js";
+import { useState } from "react";
 
 
 const Dashboard = () => {
+  const currentUser = useAppSelector(state => state.auth.user)
+
+  let isTodayACheckInDay;
+  if (currentUser) {
+    const { data } = useGetHabitsByUserQuery(currentUser.id);
+
+    const checkIns = data?.habits.map(habit => habit.checkIn)
+
+    if (checkIns) {
+      isTodayACheckInDay = isTodayCheckInDay(checkIns)
+    }
+  }
+
+  const [isBannerDisplayed, setIsBannerDisplayed] = useState(isTodayACheckInDay)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setIsBannerDisplayed(false);
+  }
+
   return (
     <>
+      {isBannerDisplayed && <CTABanner isBannerDisplayed={isBannerDisplayed} handleClick={handleClick}/>}
+      <AppHeader isBannerDisplayed={isBannerDisplayed}/>
       <RightDrawer />
       <Box as="div" 
       w="100vw"
