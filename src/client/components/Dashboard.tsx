d61179959296c4d99ba26d6b568ec2d9e26f232b
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-19 21:39:21 -0800

    Removed unnecessary impotes from Dashboard.tsx

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -1,14 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import RightDrawer from "./RightDrawer.js";
 import MyHabits from "./MyHabits.js";
-import UpdateHabitButton from "./UpdateHabitButton.js";
-import HabitCard from "./HabitCard.js";
-import { 
-    useGetHabitsByUserQuery, 
-    useGetHabitByIdQuery 
-} from "../features/api.js";
-import { useAppSelector } from "../app/hooks.js";
-
 
 const Dashboard = () => {
   return (
