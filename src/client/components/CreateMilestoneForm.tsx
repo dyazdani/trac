076b1461d971e9351bf09b9c5d7c0365578f81af
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-01-27 01:29:59 -0800

    Integrated datepicker into CreateMilestoneForm.tsx

--- src/client/components/CreateMilestoneForm.tsx
+++ src/client/components/CreateMilestoneForm.tsx
@@ -6,40 +6,20 @@ import {
     DrawerHeader,
     DrawerOverlay,
     DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
     Button,
     Stack,
     FormLabel,
     Box,
-    Input,
-    IconButton,
     ButtonGroup,
     Editable,
     EditablePreview,
     EditableInput,
-    CheckboxGroup,
-    Checkbox,
     FormControl,
-    useCheckboxGroup,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuOptionGroup,
-    MenuItemOption,
     useToast
   } from '@chakra-ui/react'
 import { useAppSelector } from '../app/hooks.js';
-import { RoutineDaysArrayType } from '../../types/index.js';
-import { useCreateHabitMutation, useCreateMilestoneMutation, useCreateScheduleMutation } from '../features/api.js';
-import { DaysOfWeek } from '@knocklabs/node';
-import getBooleanRoutineDays from '../../utils/getBooleanRoutineDays.js';
-import { DayOfTheWeek } from '@prisma/client';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import { DatePicker } from '@orange_digital/chakra-datepicker';
-import { isNullOrUndefined } from 'util';
-
+import { SingleDatepicker } from 'chakra-dayzed-datepicker';
+import { useCreateMilestoneMutation } from '../features/api.js';
 
 export interface CreateMilestoneFormProps {
     isOpenForMilestone: boolean
@@ -47,20 +27,10 @@ export interface CreateMilestoneFormProps {
 }
 
 const CreateMilestoneForm = ({isOpenForMilestone, onCloseForMilestone}: CreateMilestoneFormProps) => {
-    const [menuValue, setMenuValue] = useState<string | string[]>('Monday')
-    const [datePickerValue, setDatePickerValue] = useState<Date | null>(null)
+    const [datepickerValue, setDatepickerValue] = useState<Date | null>(null)
     const [milestoneNameValue, setMilestoneNameValue] = useState("")
 
-
-    const [createMilestone, {isLoading, data, error}] = useCreateMilestoneMutation();
-    const [
-        createSchedule, 
-        {
-            isLoading: isScheduleLoading,
-            data: scheduleData,
-            error: scheduleError
-        }
-    ] = useCreateScheduleMutation();
+    const [createMilestone] = useCreateMilestoneMutation();
 
     const inputRef = React.useRef<HTMLInputElement>(null);
     const toast = useToast();
@@ -69,126 +39,109 @@ const CreateMilestoneForm = ({isOpenForMilestone, onCloseForMilestone}: CreateMi
 
     return (
         <Drawer 
-                placement='right' 
-                onClose={onCloseForMilestone} 
-                isOpen={isOpenForMilestone}
-                closeOnEsc={false}
-                closeOnOverlayClick={false}
-                size="sm"
-                initialFocusRef={inputRef}
+            placement='right' 
+            onClose={onCloseForMilestone} 
+            isOpen={isOpenForMilestone}
+            closeOnEsc={false}
+            closeOnOverlayClick={false}
+            size="sm"
+            initialFocusRef={inputRef}
+        >
+            <DrawerOverlay />
+            <DrawerContent>
+            <DrawerHeader 
+                borderBottomWidth='1px'
             >
-                <DrawerOverlay />
-                <DrawerContent>
-                <DrawerHeader 
-                    borderBottomWidth='1px'
+                Create a Milestone
+            </DrawerHeader>
+            <DrawerBody>
+                <Stack
+                    as="form"
+                    onSubmit={async (e) => {
+                        e.preventDefault();
+                        if (currentUser && datepickerValue) {
+                            try {
+                                    const { milestone } = await createMilestone({
+                                        ownerId: currentUser.id,
+                                        name: milestoneNameValue,
+                                        dueDate: datepickerValue
+                                    }).unwrap()
+
+                                    console.log(milestone)
+
+                                    onCloseForMilestone()
+
+                                    toast({
+                                        title: 'Milestone created.',
+                                        description: 'Your new Milestone was created and added to your dashboard.',
+                                        status: 'success',
+                                        duration: 9000,
+                                        isClosable: true
+                                    })  
+                            } catch (e) {
+                                console.error(e)
+                            }
+                        }
+                    }}
+                    id="milestoneForm"
+                    spacing="3vw"
                 >
-                    Create a Milestone
-                </DrawerHeader>
-                <DrawerBody>
-                    <Stack
-                        as="form"
-                        // onSubmit={async (e) => {
-                        //     e.preventDefault();
-                        //     if (
-                        //         currentUser && 
-                        //         typeof menuValue === 'string' && 
-                        //         checkboxGroupValue &&
-                        //         !checkboxGroupValue.some(el => typeof el === 'number')
-                        //     ) {
-                        //         try {
-                        //             const { schedules } = await createSchedule({
-                        //                 milestoneName: milestoneNameValue,
-                        //                 days: [DaysOfWeek[menuValue.slice(0, 3) as keyof typeof DaysOfWeek]],
-                        //                 workflowKey: "check-in-day"
-                        //             }).unwrap()
-
-                        //             if (!scheduleError && schedules) {
-                        //                 const { milestone } = await createMilestone({
-                        //                     ownerId: currentUser.id,
-                        //                     milestoneDetails: {
-                        //                         name: milestoneNameValue,
-                        //                         routineDays: getBooleanRoutineDays(checkboxGroupValue),
-                        //                         checkInDay:  DayOfTheWeek[menuValue.toUpperCase() as keyof typeof DayOfTheWeek],
-                        //                         scheduleId: schedules[0].id
-                        //                     }
-                        //                 }).unwrap()
-
-                        //                 onClose()
-
-                        //                 toast({
-                        //                     title: 'Milestone created.',
-                        //                     description: 'Your new Milestone was created and added to your dashboard.',
-                        //                     status: 'success',
-                        //                     duration: 9000,
-                        //                     isClosable: true
-                        //                 })
-                        //             }   
-                        //         } catch (e) {
-                        //             console.error(e)
-                        //         }
-                        //     }
-                        // }}
-                        id="milestoneForm"
-                        spacing="3vw"
-                    >
-                        <Box>
-                            <FormControl isRequired>
-                                <FormLabel
-                                    htmlFor="milestoneName"
-                                >
-                                    Name
-                                </FormLabel>
-                                <Editable
-                                    defaultValue='New Milestone'
-                                >
-                                    <EditablePreview />
-                                    <EditableInput 
-                                        id="milestoneName" 
-                                        ref={inputRef}
-                                        onChange={(e) => {setMilestoneNameValue(e.target.value)}}
-                                        value={milestoneNameValue}
-                                    />
-                                </Editable>
-                            </FormControl>
-                            
-                        </Box>
-                        <Box>
+                    <Box>
                         <FormControl isRequired>
                             <FormLabel
-                                htmlFor="dueDate"
+                                htmlFor="milestoneName"
                             >
-                                Due Date</FormLabel>
-                            <DatePicker
-                                initialValue={new Date()}
-                                onDateChange={(e) => setDatePickerValue(e)}
-                            />
-                        </FormControl>
-                            
-                        </Box>
-                    </Stack>
-                </DrawerBody>
-                <DrawerFooter>
-                    <ButtonGroup>
-                        <Button 
-                            variant="outline" 
-                            colorScheme='teal' 
-                            mr={3} 
-                            onClick={onCloseForMilestone}
+                                Name
+                            </FormLabel>
+                            <Editable
+                                defaultValue='New Milestone'
+                            >
+                                <EditablePreview />
+                                <EditableInput 
+                                    id="milestoneName" 
+                                    ref={inputRef}
+                                    onChange={(e) => {setMilestoneNameValue(e.target.value)}}
+                                    value={milestoneNameValue}
+                                />
+                            </Editable>
+                        </FormControl>  
+                    </Box>
+                    <Box>
+                    <FormControl isRequired>
+                        <FormLabel
+                            htmlFor="dueDate"
                         >
-                            Cancel
-                        </Button>
-                        <Button 
-                            mr={3}  
-                            colorScheme='teal' 
-                            type="submit"
-                            form="milestoneForm"
-                        >
-                            Create
-                        </Button>
-                    </ButtonGroup>
-                </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
+                            Due Date</FormLabel>
+                        <SingleDatepicker
+                            date={new Date()}
+                            onDateChange={setDatepickerValue}
+                        />
+                    </FormControl>
+                    </Box>
+                </Stack>
+            </DrawerBody>
+            <DrawerFooter>
+                <ButtonGroup>
+                    <Button 
+                        variant="outline" 
+                        colorScheme='teal' 
+                        mr={3} 
+                        onClick={onCloseForMilestone}
+                    >
+                        Cancel
+                    </Button>
+                    <Button 
+                        mr={3}  
+                        colorScheme='teal' 
+                        type="submit"
+                        form="milestoneForm"
+                    >
+                        Create
+                    </Button>
+                </ButtonGroup>
+            </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     )
 }
 
