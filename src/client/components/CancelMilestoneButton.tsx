b8fa68abe16ab337ae658402fb327d24e1cfea0d
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-07 13:15:59 -0800

    CancelMilestoneButton disables if Milestone is completed

--- src/client/components/CancelMilestoneButton.tsx
+++ src/client/components/CancelMilestoneButton.tsx
@@ -72,6 +72,7 @@ const CancelMilestoneButton = ({milestone}: CancelMilestoneButtonProps) => {
             <IconButton 
                 aria-label="cancel-milestone-button" 
                 icon={<NotAllowedIcon />} 
+                isDisabled={milestone?.isCompleted} 
                 variant="unstyled"
                 onClick={(e) => {
                     e.preventDefault();
