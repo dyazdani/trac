38c31cc2e8295dd2a5913d1645d7c3690b460298
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-10 10:06:36 -0800

    Added getPasswordValidation helper function

--- src/utils/getPasswordValidation.ts
+++ src/utils/getPasswordValidation.ts
@@ -2,9 +2,39 @@ import { passwordStrength } from 'check-password-strength';
 
 
 const getPasswordValidation = (password: string) => {
+    const strengthCheck = passwordStrength(password);
+    
+    const isTooWeak = passwordStrength(password).value === "Too weak" || passwordStrength(password).value === "Weak"
+    
+    let containsString = ""
+    
+    strengthCheck.contains.forEach((word, i) => {
+        if (i === 0) {
+            containsString += word;
+        } else {
+            containsString += `, ${word}`
+        }
+    })
+
+    let tooWeakMessage = "";
+
+    if (isTooWeak) {
+        if (strengthCheck.contains.length < 4) {
+            tooWeakMessage += `Add the following character types to increase strength: ${containsString}. `
+        }
+
+        if (strengthCheck.length < 8) {
+            tooWeakMessage += "Increase length of password to at least 8 characters."
+        }
+    }
+
+
+
+
     const validation = {
-        isInvalid: passwordStrength(password).value === "Too weak" || passwordStrength(password).value === "Weak",
-        message: `Your password strength is: ${passwordStrength(password).value}`
+        isTooWeak,
+        message: `Your password strength is: ${strengthCheck.value}.`,
+        tooWeakMessage  
     }
 
     return validation
