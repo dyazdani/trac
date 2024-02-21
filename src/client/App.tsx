61dfe91484450be0fb5b91442150d07482ef52e7
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-21 13:27:13 -0800

    Created /goals route

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage.js';
 import Dashboard from './components/Dashboard.js';
-import { useAppSelector } from './app/hooks.js';
 
 const App: React.FC = () => {
-  const currentUser = useAppSelector((state) => state.auth.user);
-
     return (
       <>
         <Routes>
-          <Route path="/" element={currentUser ? <Dashboard /> : <LandingPage />}/>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/goals" element={<Dashboard/>}/>
         </Routes>
       </>
     );
