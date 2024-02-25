d2c24c5a75f21991ca81b2420b8e9ec1193faa91
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-25 05:17:19 -0800

    Added more screenshots and copy

--- src/client/components/LandingPage.tsx
+++ src/client/components/LandingPage.tsx
@@ -76,101 +76,181 @@ const LandingPage = () => {
                     
                 </Box>
             </VStack>
-                <VStack
-                    w="100vw"
-                    maxW="100%"
-                    p="4vw"
-                    bgColor="orange.50"
+            <VStack
+                w="100vw"
+                maxW="100%"
+                p="4vw"
+                bgColor="orange.50"
+            >
+                <Heading 
+                    as="h2" 
+                    textAlign="center"
+                    pb="4vw"
+                    w="60vw"
                 >
-                    <Heading 
-                        as="h2" 
-                        textAlign="center"
-                        pb="4vw"
-                        w="60vw"
+                    <Highlight
+                        query={['Goals', 'Goal', 'Habits', 'Habit']}
+                        styles={{
+                            color: '#979EF6'
+                        }}
                     >
-                        <Highlight
-                            query={['Goals', 'Goal', 'Habits', 'Habit']}
-                            styles={{
-                                color: '#979EF6'
-                            }}
-                        >
-                            Create Goals and the Habits required to meet them. Check off each day you practice your Habit. Mark your Goal as complete when it's finished. 
-                        </Highlight>
-                    </Heading>
-                    <Flex
-                        justifyContent="center"
+                        Create Goals and the Habits required to meet them. Check off each day you practice your Habit. Mark your Goal as complete when it's finished. 
+                    </Highlight>
+                </Heading>
+                <Flex
+                    justifyContent="center"
+                >
+                    <Card
+                        boxShadow="2xl"
+                        height="fit-content"
+                        width="fit-content"
                     >
-                        <Card
-                            boxShadow="2xl"
-                            height="fit-content"
-                            width="fit-content"
-                        >
-                            <Image
-                                src="/images/trac_screenshot.jpg"
-                                alt="trac screenshot"
-                                maxH="80vh"
-                                minH="20vh"
-                            />
-                        </Card>
-                        <Spacer
-                            minW="6vw"
-                            maxW="10vw"
+                        <Image
+                            src="/images/trac_screenshot.jpg"
+                            alt="trac screenshot"
+                            maxH="60vh"
+                            minH="40vh"
                         />
-                        <Card
-                            p="2vw"
-                            boxShadow="2xl"
-                            bgColor="#fef8e6"
-                            h="fit-content"
-                        >
-
-                            <List
-                                spacing="1vw"
-                            >
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Assign a due date for each Goal.
-                                </ListItem>
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Choose which days of the week to practice each Habit. 
-                                </ListItem>
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Record your progress. 
-                                </ListItem>
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Make revisions as needed, even to past days.
-                                </ListItem>
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Complete or cancel your Goal.
-                                </ListItem>
-                                <ListItem fontSize="2xl">
-                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
-                                    Edit or delete Habits and/or Goals at any time.
-                                </ListItem>
-                            </List>
-                        </Card>
-                    </Flex>
-                    <Heading 
-                        as="h2" 
-                        textAlign="center"
-                        pb="4vw"
-                        pt="4vw"
-                        w="60vw"
+                    </Card>
+                    <Spacer
+                        minW="6vw"
+                        maxW="10vw"
+                    />
+                    <Card
+                        p="2vw"
+                        boxShadow="2xl"
+                        bgColor="#fef8e6"
+                        h="fit-content"
                     >
-                        <Highlight
-                            query={['Progress Report', 'Check-In Day', 'social accountability']}
-                            styles={{
-                                color: '#979EF6'
-                            }}
+
+                        <List
+                            spacing="1vw"
                         >
-                            Email a Progress Report to your friends every week on your Habit's Check-In Day. Research shows that social accountability works. 
-                        </Highlight>
-                    </Heading>
-                </VStack>
-            
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Assign a due date for each Goal.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Choose which days of the week to practice each Habit. 
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Record your progress. 
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Make revisions as needed, even to past days.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Complete or cancel your Goal.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                Edit or delete Habits and/or Goals at any time.
+                            </ListItem>
+                        </List>
+                    </Card>
+                </Flex>
+
+                <Heading 
+                    as="h2" 
+                    textAlign="center"
+                    pb="4vw"
+                    pt="5vw"
+                    w="60vw"
+                >
+                    <Highlight
+                        query={['Status Report', 'Check-In Day', 'social accountability']}
+                        styles={{
+                            color: 'orange.500'
+                        }}
+                    >
+                        Email a Status Report to your friends every week on your Habit's Check-In Day. Research shows that social accountability works. 
+                    </Highlight>
+                </Heading>
+
+                <Flex
+                    justifyContent="center"
+                >
+                    <Card
+                        p="2vw"
+                        boxShadow="2xl"
+                        bgColor="#fef8e6"
+                        h="fit-content"
+                        maxW="50vw"
+                    >
+                        <List
+                            spacing="1vw"
+                        >
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="orange.500" boxSize="1.6rem" mr="1rem"/>
+                                <Text as="b">Send Status Report</Text> button appears when it is a Check-In Day.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="orange.500" boxSize="1.6rem" mr="1rem"/>
+                                Receive in-app and email reminders to send status reports as well.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="orange.500" boxSize="1.6rem" mr="1rem"/>
+                                Editable text pre-populates the status report form, making it easy to compose a message quickly. 
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="orange.500" boxSize="1.6rem" mr="1rem"/>
+                                Add emails of everyone you want to send status report to and click <Text as="b">Send</Text>.
+                            </ListItem>
+                        </List>
+                    </Card>
+                    <Spacer
+                        minW="6vw"
+                        maxW="10vw"
+                    />
+                    <Card
+                        boxShadow="2xl"
+                        height="fit-content"
+                        width="fit-content"
+                    >
+                        <Image
+                            src="/images/status_report_button_screenshot.png"
+                            alt="status report button screenshot"
+                            maxH="80vh"
+                            minH="50vh"
+                        />
+                    </Card>
+                </Flex>
+
+                <Card
+                    boxShadow="2xl"
+                    height="fit-content"
+                    width="fit-content"
+                    mt="5vw"
+                >
+                    <Image
+                        src="/images/status_report_form_drawer_screenshot.png"
+                        alt="status report form drawer screenshot"
+                        minW="70vw"
+                    />
+                </Card>
+                <Heading 
+                    as="h2" 
+                    textAlign="center"
+                    pb="3vw"
+                    pt="4vw"
+                    w="60vw"
+                >
+                    <Text color="#979EF6" mb="2vw">Dream.</Text>
+                    <Text color="green.500" mb="2vw">Achieve.</Text> 
+                    <Text color="orange.500" mb="2vw">Inspire.</Text>
+                    trac 
+                </Heading>
+                <Image
+                        src="/images/trac_logo.png"
+                        alt="trac mountain logo"
+                        
+                    />
+                
+            </VStack>     
         </>   
     )
 }
