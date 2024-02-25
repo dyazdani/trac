4d2907a42abf382578b189e17f41f34f479f4039
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-24 21:41:16 -0800

    Added new landing page with the beginnings of design

--- src/client/components/LandingPage.tsx
+++ src/client/components/LandingPage.tsx
@@ -1,53 +1,106 @@
-import { useState } from "react";
-import { Image, Flex, Spacer, Hide } from "@chakra-ui/react";
-
-import RegisterForm from "./RegisterForm.js";
-import LoginForm from "./LoginForm.js";
+import { 
+    Box,
+    Button,
+    HStack,
+    Heading, 
+    Spacer, 
+    VStack,
+    Image,
+    Card,
+    Flex,
+    List,
+    ListItem,
+    ListIcon,
+    Icon,
+    Text
+} from "@chakra-ui/react";
+import AppHeader from "./AppHeader.js";
+import { ChevronRightIcon, CheckCircleIcon } from "@chakra-ui/icons";
 
 const LandingPage = () => {
-  const [isLoginShowing, setIsLoginShowing] = useState(false);
+    return (
+        <>
+            <AppHeader isBannerDisplayed/>
+            <VStack
+                h="100vh                                                                                                                                            0vh)"
+                w="100vw"
+                maxWidth="100%"
+                p="1vw"
+                alignItems="center"
+            >
+                <Heading
+                    as="h1"
+                    size="2xl"
+                    mt="5vw"
+                >
+                    Your secret, goal-achieving weapon
+                </Heading>
+                <Heading
+                    as="h2"
+                    size="lg"
+                    mt="2vw"
+                >
+                    Track you habits. Share your progress. Crush your goals.
+                </Heading>
+                <Button
+                    rightIcon={<ChevronRightIcon/>}
+                    colorScheme="blue"
+                    mt="7vw"
+                >
+                    Get Started
+                </Button>
 
-  const handleLinkClick = () => {
-    setIsLoginShowing(!isLoginShowing);
-  };
+            </VStack>
+                <Box
+                    w="100vw"
+                    maxW="100%"
+                    mt="2vw"
+                    p="5vw"
+                    bgColor="#eaf7fa"
+                >
+                <Flex
 
-  return (
-    <Flex 
-        alignItems="center" 
-        w="100vw" 
-        h="100vh" 
-        bg="#b9eefe"
-    >
-      <Spacer />
-      <Hide breakpoint="(max-width: 600px)">
-        <Image
-          src="/images/mountain-climber.jpg"
-          alt="mountain climber scaling mountain"
-          maxW="50vw"
-          maxH="100%"
-          h="100vh"
-          objectFit="cover"
-          borderRadius="50%"
-          border="5px"
-          bgColor="#564740"
-          p="4"
-          boxSize="35vw"
-        />
-        <Spacer />
-        <Spacer />
-      </Hide>
+                >
+                    <Card
+                        boxShadow="2xl"
+                        height="fit-content"
+                        width="fit-content"
+                    >
+                        <Image
+                            src="/images/trac_screenshot.jpg"
+                            alt="trac screenshot"
+                            maxH="80vh"
+                            minH="20vh"
+                        />
+                    </Card>
+                    <Spacer
+                        minW="5vw"
+                    />
+                    <Card
+                        p="2vw"
+                        boxShadow="2xl"
+                        bgColor="#fef8e6"
+                        h="fit-content"
+                    >
 
-      {isLoginShowing ? (
-        <LoginForm handleLinkClick={handleLinkClick} />
-      ) : (
-        <RegisterForm handleLinkClick={handleLinkClick} />
-      )}
-      <Hide breakpoint="(max-width: 600px)">
-        <Spacer />
-      </Hide>
-      <Spacer />
-    </Flex>
-  );
-};
+                        <List
+                            spacing="1vw"
+                        >
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="green" boxSize="1.6rem" mr="1rem"/>
+                                Create goals with due dates.
+                            </ListItem>
+                            <ListItem fontSize="2xl">
+                                <ListIcon as={CheckCircleIcon} color="green" boxSize="1.6rem" mr="1rem"/>
+                                Track the habits needed to complete your goals.
+                            </ListItem>
+                        </List>
+                    </Card>
+                </Flex>
+                </Box>
+            
+        </>   
+    )
+}
 
 export default LandingPage;
