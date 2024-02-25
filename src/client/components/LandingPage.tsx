7a8f67467aefd36149c24914657740e6a55ca5a7
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-25 02:46:11 -0800

    Added and formatted copy for Landing Page

--- src/client/components/LandingPage.tsx
+++ src/client/components/LandingPage.tsx
@@ -1,7 +1,6 @@
 import { 
     Box,
     Button,
-    HStack,
     Heading, 
     Spacer, 
     VStack,
@@ -10,7 +9,9 @@ import {
     Flex,
     List,
     ListItem,
-    ListIcon
+    ListIcon,
+    Text,
+    Highlight
 } from "@chakra-ui/react";
 import AppHeader from "./AppHeader.js";
 import { 
@@ -45,14 +46,13 @@ const LandingPage = () => {
                     >
                         Your secret, goal-achieving weapon
                     </Heading>
-                    <Heading
-                        as="h2"
-                        size="lg"
+                    <Text
+                        fontSize="xl"
                         mt="2vw"
                         textAlign="center"
                     >
                         Track your habits. Share your progress. Crush your goals.
-                    </Heading>
+                    </Text>
                     <Flex
                         direction="column"
                         w="100%"
@@ -62,8 +62,8 @@ const LandingPage = () => {
                             rightIcon={<ChevronRightIcon/>}
                             colorScheme="orange"
                             color="#000000"
-                            mt="10vw"
-                            mb="3vw"
+                            mt="8vw"
+                            mb="4vw"
                             type="button"
                             onClick={(e) => {
                                 e.preventDefault();
@@ -76,55 +76,100 @@ const LandingPage = () => {
                     
                 </Box>
             </VStack>
-                {/* <Heading as="h3" size="md" >Create goals and</Heading> */}
-                <Box
+                <VStack
                     w="100vw"
                     maxW="100%"
-                    mt="2vw"
                     p="4vw"
-                    bgColor="#eaf7fa"
+                    bgColor="orange.50"
                 >
-                <Flex
-                    justifyContent="center"
-                >
-                    <Card
-                        boxShadow="2xl"
-                        height="fit-content"
-                        width="fit-content"
+                    <Heading 
+                        as="h2" 
+                        textAlign="center"
+                        pb="4vw"
+                        w="60vw"
                     >
-                        <Image
-                            src="/images/trac_screenshot.jpg"
-                            alt="trac screenshot"
-                            maxH="80vh"
-                            minH="20vh"
-                        />
-                    </Card>
-                    <Spacer
-                        minW="4vw"
-                        maxW="10vw"
-                    />
-                    <Card
-                        p="2vw"
-                        boxShadow="2xl"
-                        bgColor="#fef8e6"
-                        h="fit-content"
-                    >
-
-                        <List
-                            spacing="1vw"
+                        <Highlight
+                            query={['Goals', 'Goal', 'Habits', 'Habit']}
+                            styles={{
+                                color: '#979EF6'
+                            }}
                         >
-                            <ListItem fontSize="2xl">
-                                <ListIcon as={CheckCircleIcon} color="green" boxSize="1.6rem" mr="1rem"/>
-                                Create goals with due dates.
-                            </ListItem>
-                            <ListItem fontSize="2xl">
-                                <ListIcon as={CheckCircleIcon} color="green" boxSize="1.6rem" mr="1rem"/>
-                                Track the habits needed to complete your goals.
-                            </ListItem>
-                        </List>
-                    </Card>
-                </Flex>
-                </Box>
+                            Create Goals and the Habits required to meet them. Check off each day you practice your Habit. Mark your Goal as complete when it's finished. 
+                        </Highlight>
+                    </Heading>
+                    <Flex
+                        justifyContent="center"
+                    >
+                        <Card
+                            boxShadow="2xl"
+                            height="fit-content"
+                            width="fit-content"
+                        >
+                            <Image
+                                src="/images/trac_screenshot.jpg"
+                                alt="trac screenshot"
+                                maxH="80vh"
+                                minH="20vh"
+                            />
+                        </Card>
+                        <Spacer
+                            minW="6vw"
+                            maxW="10vw"
+                        />
+                        <Card
+                            p="2vw"
+                            boxShadow="2xl"
+                            bgColor="#fef8e6"
+                            h="fit-content"
+                        >
+
+                            <List
+                                spacing="1vw"
+                            >
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Assign a due date for each Goal.
+                                </ListItem>
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Choose which days of the week to practice each Habit. 
+                                </ListItem>
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Record your progress. 
+                                </ListItem>
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Make revisions as needed, even to past days.
+                                </ListItem>
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Complete or cancel your Goal.
+                                </ListItem>
+                                <ListItem fontSize="2xl">
+                                    <ListIcon as={CheckCircleIcon} color="#979EF6" boxSize="1.6rem" mr="1rem"/>
+                                    Edit or delete Habits and/or Goals at any time.
+                                </ListItem>
+                            </List>
+                        </Card>
+                    </Flex>
+                    <Heading 
+                        as="h2" 
+                        textAlign="center"
+                        pb="4vw"
+                        pt="4vw"
+                        w="60vw"
+                    >
+                        <Highlight
+                            query={['Progress Report', 'Check-In Day', 'social accountability']}
+                            styles={{
+                                color: '#979EF6'
+                            }}
+                        >
+                            Email a Progress Report to your friends every week on your Habit's Check-In Day. Research shows that social accountability works. 
+                        </Highlight>
+                    </Heading>
+                </VStack>
             
         </>   
     )
