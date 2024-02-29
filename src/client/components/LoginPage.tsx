2062e330c889bb8b00619138f20e3ec25fcc6d7a
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-02-28 23:30:08 -0800

    Added warning about app not being optimized for mobile to Login Pagee

--- src/client/components/LoginPage.tsx
+++ src/client/components/LoginPage.tsx
@@ -2,42 +2,59 @@ import {
     Image, 
     Flex, 
     Spacer, 
-    Hide 
+    Hide, 
+    Show,
+    Heading
   } from "@chakra-ui/react";
 import LoginForm from "./LoginForm.js";
 
 const LoginPage = () => {
     return (
-        <Flex 
-            alignItems="center" 
-            w="100vw" 
-            h="100vh" 
-            bg="#b9eefe"
-        >
-            <Spacer/>
-            <Hide breakpoint="(max-width: 600px)">
-                <Image
-                    src="/images/mountain-climber.jpg"
-                    alt="mountain climber scaling mountain"
-                    maxW="50vw"
-                    maxH="100%"
-                    h="100vh"
-                    objectFit="cover"
-                    borderRadius="50%"
-                    border="5px"
-                    bgColor="#564740"
-                    p="4"
-                    boxSize="35vw"
-                />
+        <>
+            <Show 
+                breakpoint="(max-width: 768px)"
+            >
+                <Heading 
+                    as="h1" 
+                    size="lg" 
+                    textAlign="center" 
+                    backgroundColor="yellow.500"
+                    padding="1vw"
+                >
+                    Trac not yet optimized for tablet or mobile devices. Please switch to desktop for optimum experience.
+                </Heading>
+            </Show>
+            <Flex 
+                alignItems="center" 
+                w="100vw" 
+                h="100vh" 
+                bg="#b9eefe"
+            >
                 <Spacer/>
+                <Hide breakpoint="(max-width: 600px)">
+                    <Image
+                        src="/images/mountain-climber.jpg"
+                        alt="mountain climber scaling mountain"
+                        maxW="50vw"
+                        maxH="100%"
+                        h="100vh"
+                        objectFit="cover"
+                        borderRadius="50%"
+                        border="5px"
+                        bgColor="#564740"
+                        p="4"
+                        boxSize="35vw"
+                    />
+                    <Spacer/>
+                    <Spacer/>
+                </Hide>
+                <LoginForm/>
+                <Hide breakpoint="(max-width: 600px)">
+                    <Spacer />
+                </Hide>
                 <Spacer/>
-            </Hide>
-            <LoginForm/>
-            <Hide breakpoint="(max-width: 600px)">
-                <Spacer />
-            </Hide>
-            <Spacer/>
-        </Flex>
+            </Flex>
+        </>
     )
 }
 
