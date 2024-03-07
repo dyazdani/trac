593d288e97e4d00cea71adeecbfcc9f3ed6a5349
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-03-07 02:31:11 -0800

    Changed color of delete habit button in habit menu list

--- src/client/components/DeleteHabitButton.tsx
+++ src/client/components/DeleteHabitButton.tsx
@@ -60,7 +60,14 @@ const DeleteHabitButton = ({ habit}: DeleteHabitButtonProps) =>  {
     
     return(
         <MenuItem 
-            aria-label="Delete Habit" 
+            aria-label="Delete Habit"
+            backgroundColor="turquoise.50"
+            _hover={{
+                backgroundColor: "turquoise.100"
+            }}
+            _active={{
+                backgroundColor: "turquoise.200"
+            }}
             icon={<DeleteIcon />}
             onClick={(e) => {
                 e.preventDefault();
