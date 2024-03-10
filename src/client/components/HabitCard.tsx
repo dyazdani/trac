73ec19b1065b4fb6d2a396ba221b9bc3635cdb88
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-03-09 22:49:58 -0800

    Changed color of animated shine background for Habit with a due Status Report

--- src/client/components/HabitCard.tsx
+++ src/client/components/HabitCard.tsx
@@ -184,7 +184,7 @@ const HabitCard = ({ habit, milestone }: HabitProps) => {
       minW="424px"
       bg={
         milestone && milestone.isCanceled ? "#CDCBCB" :
-        !isStatusReportSent && !isTodayBeforeFirstCheckInDayDate ? "linear-gradient(-45deg, #F9D162 40%, #FBE29D 50%, #F9D162 60%)" : "#C9E5F6"
+        !isStatusReportSent && !isTodayBeforeFirstCheckInDayDate ? "linear-gradient(-45deg, #C9E5F6 40%, #DCEEF9 50%, #C9E5F6 60%)" : "#C9E5F6"
       }
       borderRadius="20px"
       border={milestone && milestone.isCompleted || milestone.isCanceled ? "" :
