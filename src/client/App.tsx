1de2f47f2dab78184f31c395ac3f3458c8372c13
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-03-12 01:55:23 -0700

    going to /login route when logged in redirects to /goals route

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage isAuthenticated={!!currentUser}/>}/>
         <Route path="goals" element={<Dashboard isAuthenticated={!!currentUser}/>}/>
         <Route path="register" element={<Register/>}/>
-        <Route path="login" element={<Login/>}/>
+        <Route path="login" element={<Login isAuthenticated={!!currentUser}/>}/>
       </Routes>
     </>
   );
