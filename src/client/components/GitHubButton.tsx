8be8b1811e832a60c5a9156e7f019de16762fd7d
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-04-02 15:19:12 -0700

    Added GitHubButton component

---
+++ src/client/components/GitHubButton.tsx
@@ -0,0 +1,51 @@
+import { 
+    Button, 
+    Flex, 
+    Image, 
+    LinkBox,
+    LinkOverlay
+} from "@chakra-ui/react";
+
+
+const GitHubButton = () => {
+    return (
+        <LinkBox
+            as="button"
+            backgroundColor="#FFFFFF"
+            padding=".2rem"
+            borderRadius="8px"
+            height="40px"
+            _hover={{
+                backgroundColor: "#c2bebd"
+            }}
+            _active={{
+                backgroundColor: "#918a88"
+            }}
+            border="1px #0d0d0c solid"
+            >
+            <LinkOverlay
+                isExternal={true}
+                href="https://github.com/dyazdani/trac"
+            >
+                <Flex
+                    alignItems="center"
+                    justifyContent="center"
+                    height="100%"
+                >
+                    <Image
+                        src="/images/github-mark.png"
+                        alt="GitHub mark"
+                        height="100%"
+                    />
+                    <Image
+                        src="/images/GitHub_Logo.png"
+                        alt="GitHub logo"
+                        height="100%"
+                    />
+                </Flex>
+            </LinkOverlay>
+        </LinkBox>
+    )
+}
+
+export default GitHubButton;
