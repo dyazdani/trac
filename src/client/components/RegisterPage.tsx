99c75aee12e6cb5847a581377e7d7a04fd0feee3
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-09-30 14:43:44 -0700

    removed responsiveness warning banner from RegisterPage.tsx

--- src/client/components/RegisterPage.tsx
+++ src/client/components/RegisterPage.tsx
@@ -12,38 +12,6 @@ import GitHubButton from "./GitHubButton.js";
   const RegisterPage = () => {
     return (
       <>
-        <Hide below="md">
-          <Show breakpoint="(max-height: 660px)">
-            <Heading 
-              as="h1" 
-              size="lg" 
-              textAlign="center" 
-              backgroundColor="gold.400"
-              padding="1vw"
-              position="sticky"
-              top="0"
-              zIndex={2}
-            >
-              Trac not yet optimized for tablet or mobile devices. Please switch to desktop for optimum experience.
-            </Heading>
-          </Show>
-        </Hide>
-        <Show 
-          below="md"
-        >
-          <Heading 
-            as="h1" 
-            size="lg" 
-            textAlign="center" 
-            backgroundColor="gold.400"
-            padding="1vw"
-            position="sticky"
-            top="0"
-            zIndex={2}
-          >
-            Trac not yet optimized for tablet or mobile devices. Please switch to desktop for optimum experience.
-          </Heading>
-        </Show>
         <Flex 
           alignItems="center" 
           justifyContent="center"
