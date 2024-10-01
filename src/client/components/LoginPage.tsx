e507a1f868f0ecd630834cac7cbd0f4843639cf3
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-10-01 16:17:15 -0700

    Added ArtistCredit component to LoginPage

--- src/client/components/LoginPage.tsx
+++ src/client/components/LoginPage.tsx
@@ -1,10 +1,6 @@
-import { 
-    Flex, 
-    Text,
-    Link,
-  } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import LoginForm from "./LoginForm.js";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import ArtistCredit from "./ArtistCredit.js";
 
 const LoginPage = () => {
     return (
@@ -17,7 +13,7 @@ const LoginPage = () => {
                 justifyContent="center"
                 w="100%" 
                 h="100dvh" 
-                minHeight="650px"
+                minHeight="660px"
                 bgImage="url('/images/landing_page_bg_image.jpg')"
                 bgPosition="bottom"
                 bgRepeat="no-repeat"
@@ -25,11 +21,9 @@ const LoginPage = () => {
             >           
                 <LoginForm/>
             </Flex>
-            <Text fontSize="sm" justifyContent="center" position="absolute" bottom="0" left="5px" color="blue.50">
-                <Link href="https://thenounproject.com/icon/mountain-120042/" isExternal>Mountain<ExternalLinkIcon mr=".5em" boxSize=".9em" /></Link>
-                by <Link href="https://thenounproject.com/bravo/" isExternal>Juan Pablo Bravo<ExternalLinkIcon mr=".3em" boxSize="1em"/></Link> 
-                is licensed under <Link href="https://creativecommons.org/licenses/by/2.0/" isExternal>CC BY 2.0<ExternalLinkIcon mr=".5em" boxSize="1em"/></Link>
-            </Text>
+            <ArtistCredit 
+                textColor="blue.50" 
+            />
         </>
     )
 }
