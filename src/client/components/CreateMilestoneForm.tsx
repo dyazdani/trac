7c5bd3482b1ec8e521177f8ae7d7d134915976d3
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-10-03 12:03:33 -0700

    Made CreateMilestoneForm on Dashboard responsive

--- src/client/components/CreateMilestoneForm.tsx
+++ src/client/components/CreateMilestoneForm.tsx
@@ -52,125 +52,135 @@ const CreateMilestoneForm = ({isOpenForMilestone, onCloseForMilestone}: CreateMi
             initialFocusRef={inputRef}
         >
             <DrawerOverlay />
-            <DrawerContent>
-            <DrawerHeader 
-                borderBottomWidth='1px'
+            <DrawerContent
+                overflowX="hidden"
+                overflowY="auto"
             >
-                Add a Goal
-            </DrawerHeader>
-            <DrawerBody>
-                <Stack
-                    as="form"
-                    onSubmit={async (e) => {
-                        e.preventDefault();
-                        if (currentUser && datepickerValue) {
-                            try {
-                                    const { milestone } = await createMilestone({
-                                        ownerId: currentUser.id,
-                                        name: milestoneNameValue,
-                                        dueDate: datepickerValue
-                                    }).unwrap()
-
-                                    console.log(milestone)
-
-                                    onCloseForMilestone()
-
-                                    toast({
-                                        title: 'Goal Added',
-                                        description: `"${milestone.name}" was added to your dashboard.`,
-                                        status: 'success',
-                                        variant: 'subtle',
-                                        duration: 9000,
-                                        isClosable: true,
-                                        icon: <AddIcon/>
-                                    })  
-                            } catch (e) {
-                                console.error(e)
-                            }
-                        }
-                    }}
-                    id="milestoneForm"
-                    spacing="3vw"
+                <DrawerHeader 
+                    borderBottomWidth='1px'
                 >
-                    <Box>
+                    Add a Goal
+                </DrawerHeader>
+                <DrawerBody>
+                    <Stack
+                        as="form"
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            if (currentUser && datepickerValue) {
+                                try {
+                                        const { milestone } = await createMilestone({
+                                            ownerId: currentUser.id,
+                                            name: milestoneNameValue,
+                                            dueDate: datepickerValue
+                                        }).unwrap()
+
+                                        console.log(milestone)
+
+                                        onCloseForMilestone()
+
+                                        toast({
+                                            title: 'Goal Added',
+                                            description: `"${milestone.name}" was added to your dashboard.`,
+                                            status: 'success',
+                                            variant: 'subtle',
+                                            duration: 9000,
+                                            isClosable: true,
+                                            icon: <AddIcon/>
+                                        })  
+                                } catch (e) {
+                                    console.error(e)
+                                }
+                            }
+                        }}
+                        id="milestoneForm"
+                        spacing="3vw"
+                    >
+                        <Box>
+                            <FormControl isRequired>
+                                <FormLabel
+                                    htmlFor="milestoneName"
+                                >
+                                    Name
+                                </FormLabel>
+                                <Editable
+                                    defaultValue='New Goal'
+                                >
+                                    <EditablePreview />
+                                    <EditableInput 
+                                        id="milestoneName" 
+                                        ref={inputRef}
+                                        onChange={(e) => {setMilestoneNameValue(e.target.value)}}
+                                        value={milestoneNameValue}
+                                        paddingLeft="16px"
+                                        paddingRight="16px"
+                                    />
+                                </Editable>
+                            </FormControl>  
+                        </Box>
+                        <Box>
                         <FormControl isRequired>
                             <FormLabel
-                                htmlFor="milestoneName"
+                                htmlFor="dueDate"
                             >
-                                Name
-                            </FormLabel>
-                            <Editable
-                                defaultValue='New Goal'
-                            >
-                                <EditablePreview />
-                                <EditableInput 
-                                    id="milestoneName" 
-                                    ref={inputRef}
-                                    onChange={(e) => {setMilestoneNameValue(e.target.value)}}
-                                    value={milestoneNameValue}
-                                    paddingLeft="16px"
-                                    paddingRight="16px"
-                                />
-                            </Editable>
-                        </FormControl>  
-                    </Box>
-                    <Box>
-                    <FormControl isRequired>
-                        <FormLabel
-                            htmlFor="dueDate"
+                                Due Date</FormLabel>
+                            <SingleDatepicker
+                                id="dueDate"
+                                name="date-input"
+                                propsConfigs={{
+                                    popoverCompProps: {
+                                    popoverBodyProps:  {
+                                        maxWidth: "100dvw"
+                                    }
+                                    }
+                                }}
+                                date={datepickerValue}
+                                minDate={new Date()}
+                                onDateChange={(e) => {
+                                    setDatepickerValue(e)
+                                }}
+                                configs={{
+                                    dateFormat: "MM-dd-yyyy"   
+                                }}
+                            />
+                        </FormControl>
+                        </Box>
+                    </Stack>
+                </DrawerBody>
+                <DrawerFooter>
+                    <ButtonGroup>
+                        <Button 
+                            variant="outline" 
+                            colorScheme='yellow' 
+                            color="yellow.800"
+                            mr={6} 
+                            onClick={(e) => {
+                                e.preventDefault();
+                                onCloseForMilestone();
+                                setDatepickerValue(undefined);
+                                setMilestoneNameValue("New Goal");
+                            }}
                         >
-                            Due Date</FormLabel>
-                        <SingleDatepicker
-                            id="dueDate"
-                            name="date-input"
-                            date={datepickerValue}
-                            minDate={new Date()}
-                            onDateChange={(e) => {
-                                setDatepickerValue(e)
+                            Cancel
+                        </Button>
+                        <Button 
+                            mr={3}  
+                            colorScheme='yellow'
+                            _hover={{
+                                backgroundColor: "yellow.600"
                             }}
-                            configs={{
-                                dateFormat: "MM-dd-yyyy"   
-                            }}
-                        />
-                    </FormControl>
-                    </Box>
-                </Stack>
-            </DrawerBody>
-            <DrawerFooter>
-                <ButtonGroup>
-                    <Button 
-                        variant="outline" 
-                        colorScheme='yellow' 
-                        color="yellow.800"
-                        mr={6} 
-                        onClick={(e) => {
-                            e.preventDefault();
-                            onCloseForMilestone();
-                            setDatepickerValue(undefined);
-                            setMilestoneNameValue("New Goal");
-                        }}
-                    >
-                        Cancel
-                    </Button>
-                    <Button 
-                        mr={3}  
-                        colorScheme='yellow'
-                        _hover={{
-                            backgroundColor: "yellow.600"
-                        }}
-                        _active={{
-                            backgroundColor: "yellow.700"
-                        }} 
-                        type="submit"
-                        form="milestoneForm"
-                        isLoading={isLoading}
-                        paddingX={"2rem"}
-                        isDisabled={!datepickerValue || !milestoneNameValue}
-                    >
-                        Add
-                    </Button>
-                </ButtonGroup>
-            </DrawerFooter>
+                            _active={{
+                                backgroundColor: "yellow.700"
+                            }} 
+                            type="submit"
+                            form="milestoneForm"
+                            isLoading={isLoading}
+                            paddingX={"2rem"}
+                            isDisabled={!datepickerValue || !milestoneNameValue}
+                        >
+                            Add
+                        </Button>
+                    </ButtonGroup>
+                </DrawerFooter>
             </DrawerContent>
         </Drawer>
     )
