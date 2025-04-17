5bad24bcc408eb687faf1ec496c6871a76519e15
dara <dara.s.yazdani@gmail.com>
2025-04-16 19:36:40 -0700

    Changed console log mesaege to developers

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -10,8 +10,9 @@ import Login from './components/Login.js';
 import rainbowConsoleLogStyles from './constants/rainbowConsoleLogStyles.js'
 
 const App: React.FC = () => {
-  console.log("%c Hi! I'm Dara Yazdani", rainbowConsoleLogStyles)  
-  console.log("Thanks for checking out Trac! Find my other projects at https://www.darayazdani.com. Feel free to reach out or connect: https://www.linkedin.com/in/darayazdani. Cheers!") 
+  console.log("%c Welcome! d(-_☆)", rainbowConsoleLogStyles)  
+  console.log("Hi, I'm Dara. Thanks for checking Trac. Feel free to reach out if you have any questions or just want connect: https://www.linkedin.com/in/darayazdani. You can find all my projects at https://www.darayazdani.com. Cheers!") 
+  console
 
   return (
     <Routes>
